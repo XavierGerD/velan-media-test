75697bc362c0bff58322b254bf72b455274e05b4
XavierGerD <50594140+XavierGerD@users.noreply.github.com>
2020-01-08 10:28:47 -0500

    added sql file

---
+++ php/create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "test"."emploi"
+(
+    "idemploi" INT NOT NULL,
+    "job_title" VARCHAR(45) NOT NULL,
+    "location" VARCHAR(45) NOT NULL,
+    "company" VARCHAR(45) NOT NULL,
+    "image" VARCHAR(45) NOT NULL,
+    PRIMARY KEY ("idemploi")
+);
